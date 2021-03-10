a34d9054ab6f716eb046d275227849acbdfbd933
Benjamin Leray <b.leray35@gmail.com>
2021-03-10 16:58:17 +0100

    add rubocop-rails, and config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -12,4 +13,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
 end
