56ba209a4719162c23f85c5e9fbfcdd221699187
Benjamin Leray <b.leray35@gmail.com>
2021-03-11 17:18:16 +0100

    setup devise auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  #  :confirmable, :omniauthable
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  # devise_for :users
   namespace :api do
     namespace :test do
       get 'connection', action: 'connection'
