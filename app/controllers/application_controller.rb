2c5c37432eaed003339755be3d758ff2dae6a562
Benjamin Leray <b.leray35@gmail.com>
2021-03-11 18:25:50 +0100

    sign up requets hold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery unless: -> { request.format.json? }
+
 end
