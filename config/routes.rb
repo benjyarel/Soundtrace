1f2f69a42946ec38be295558770c03a2f308bce8
Benjamin Leray <b.leray35@gmail.com>
2021-03-12 11:15:54 +0100

    removing testing purposes, add another test (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # devise_for :users
   namespace :api do
-    namespace :test do
-      get 'connection', action: 'connection'
-    end
   end
 end
